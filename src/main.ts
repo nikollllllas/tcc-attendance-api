ef5878c901fcaa76e328f959a99b81c9216c9051
nikollllllas <nikollasohta2014@gmail.com>
2024-11-20 13:25:02 -0300

    auauauauau

--- src/main.ts
+++ src/main.ts
@@ -27,6 +27,7 @@ async function bootstrap() {
   const document = SwaggerModule.createDocument(app, config)
   SwaggerModule.setup("swagger", app, document)
 
-  await app.listen(process.env.PORT)
+  await app.listen(process.env.PORT || 3000)
+  console.log(`Application is running on: ${await app.getUrl()}`)
 }
 bootstrap()
