1e531a88a26aa23c34a8560692adf2c6287aafbe
nikollllllas <nikollasohta2014@gmail.com>
2024-12-02 20:12:55 -0300

    ???

--- src/app.module.ts
+++ src/app.module.ts
@@ -4,7 +4,6 @@ import { CourseModule } from "./course/course.module"
 import { StudentModule } from "./student/student.module"
 import { TeacherModule } from "./teacher/teacher.module"
 import { SubjectModule } from "./subject/subject.module"
-import { BeaconModule } from "./beacon/beacon.module"
 import { PrismaModule } from "./prisma/prisma.module"
 import { ConfigModule } from "@nestjs/config"
 import { SchoolCallModule } from "./school-call/school-call.module"
@@ -14,7 +13,6 @@ import { SchoolCallModule } from "./school-call/school-call.module"
     ConfigModule.forRoot({ isGlobal: true }),
     AuthModule,
     PrismaModule,
-    BeaconModule,
     CourseModule,
     SchoolCallModule,
     StudentModule,
