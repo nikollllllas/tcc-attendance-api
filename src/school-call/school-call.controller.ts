7bdae3dbf840e07a1fcd4455fc038b21d6a7496f
nikollllllas <nikollasohta2014@gmail.com>
2024-12-03 01:01:51 -0300

    k9wahsng0insd

--- src/school-call/school-call.controller.ts
+++ src/school-call/school-call.controller.ts
@@ -7,44 +7,44 @@ import {
   Body,
   UseGuards,
   NotFoundException,
-} from '@nestjs/common'
-import { CreateSchoolCallDto } from './dto/create-scholl-call-dto'
-import { AuthGuard } from '@nestjs/passport'
-import { ApiSecurity, ApiTags } from '@nestjs/swagger'
-import { SchoolCallService } from './school-call.service'
+} from "@nestjs/common"
+import { CreateSchoolCallDto } from "./dto/create-scholl-call-dto"
+import { AuthGuard } from "@nestjs/passport"
+import { ApiSecurity, ApiTags } from "@nestjs/swagger"
+import { SchoolCallService } from "./school-call.service"
 
-@Controller('school-call')
-@UseGuards(AuthGuard('jwt'))
-@ApiTags('Chamada Escolar')
+@Controller("school-call")
+@UseGuards(AuthGuard("jwt"))
+@ApiTags("Chamada Escolar")
 export class SchoolCallController {
   constructor(private readonly schoolCallService: SchoolCallService) {}
 
   @Post()
-  @ApiSecurity('token')
+  @ApiSecurity("token")
   async create(@Body() createSchoolCallDto: CreateSchoolCallDto) {
     return this.schoolCallService.create(createSchoolCallDto)
   }
 
-  @Get('list')
-  @ApiSecurity('token')
+  @Get("")
+  @ApiSecurity("token")
   async findAll() {
     try {
       return await this.schoolCallService.findAll()
     } catch (e) {
-      throw new NotFoundException('Error fetching school calls')
+      throw new NotFoundException("Error fetching school calls")
     }
   }
 
-  @Get('list/:id')
-  @ApiSecurity('token')
-  async findOne(@Param('id') id: number) {
+  @Get("/:id")
+  @ApiSecurity("token")
+  async findOne(@Param("id") id: number) {
     return await this.schoolCallService.findOne(+id)
   }
 
-  @Delete('delete/:id')
-  @ApiSecurity('token')
-  async delete(@Param('id') id: string) {
+  @Delete("/:id")
+  @ApiSecurity("token")
+  async delete(@Param("id") id: string) {
     await this.schoolCallService.remove(+id)
-    return { message: 'School call deleted.' }
+    return { message: "School call deleted." }
   }
 }
